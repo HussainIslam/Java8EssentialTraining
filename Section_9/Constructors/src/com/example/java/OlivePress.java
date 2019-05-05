ddebb5cf1076eae061009d38e561a289d2544228
hussain <m.hussainul.islam@gmail.com>
2019-05-05 11:43:32 -0400

    Constructors

--- Section_9/Constructors/src/com/example/java/OlivePress.java
+++ Section_9/Constructors/src/com/example/java/OlivePress.java
@@ -8,7 +8,7 @@ public class OlivePress {
     public int getOil(List<Olive> olives){
         int totalOil = 0;
         for(Olive o: olives){
-            System.out.println(o.getName);
+            System.out.println(o.getName());
             totalOil += o.crush();
         }
         return totalOil;
