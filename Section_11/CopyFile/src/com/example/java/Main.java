e73e1beb7d16060d9eef5bea0bb2fbaf7ef285d8
hussain <m.hussainul.islam@gmail.com>
2019-05-06 01:11:29 -0400

    NewIO

---
+++ Section_11/CopyFile/src/com/example/java/Main.java
@@ -0,0 +1,37 @@
+package com.example.java;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String sourceFile = "files/loremipsum.txt";
+	    String targetFile = "files/target.txt";
+
+	    // try with resouces introduced in J7
+	    try (
+	            //creating three closable objects
+                // a file reader
+                // a buffer reader
+                // a file writer
+                FileReader fReader = new FileReader(sourceFile);
+                BufferedReader bReader = new BufferedReader(fReader);
+                FileWriter writer = new FileWriter(targetFile)
+                ){
+            while(true){
+                String line = bReader.readLine();
+                if(line == null){
+                    break;
+                } else{
+                    writer.write(line + "\n");
+                }
+            }
+            System.out.println("File copied!");
+        }
+	    catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
